499411990334c0edf1eace1afc8a9d5f2cad43d3
W9pi3cZ1 <ghg13368@163.com>
2024-10-19 15:12:46 +0800

    Fixed login example

--- examples/login/src/main.rs
+++ examples/login/src/main.rs
@@ -1,4 +1,4 @@
-use openfrp_sdk::{login::*, prelude::*,user};
+use openfrp_api::{login::*, prelude::*,user};
 
 pub const EMAIL: &'static str = "xslimenb@xslimenb.eu.org";
 pub const PASSWORD: &'static str = "123Tester_";
@@ -8,10 +8,10 @@ async fn main() -> Result<()>{
     let mut client = Client::new();
     let account = Account::new(EMAIL, PASSWORD);
     login(&account, &mut client).await?;
-    println!("auth: {:#?}",client.auth.clone().get()?);
-    let userinfo = user::get_info(&mut client).await?;
+    println!("auth: {:#?}",client.get_auth()?);
+    let userinfo = user::get_user_info(&mut client).await?;
     println!("userinfo: {:#?}", userinfo);
-    let userproxies = user::get_proxies(&mut client).await?;
+    let userproxies = user::get_user_proxies(&mut client).await?;
     println!("userproxies: {:#?}", userproxies);
     Ok(())
 }
